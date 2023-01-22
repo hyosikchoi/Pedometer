b522e67b76fcfaaf423937e75bfc26b17cee8359
hyosikchoi <huchoi24@naver.com>
2023-01-23 00:52:56 +0900

    Refactor - Create Accelerometer object

--- app/src/main/java/com/hyosik/android/pedometer/MainActivity.kt
+++ app/src/main/java/com/hyosik/android/pedometer/MainActivity.kt
@@ -1,23 +1,14 @@
 package com.hyosik.android.pedometer
 
-import android.content.Context
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import com.hyosik.android.pedometer.databinding.ActivityMainBinding
-import com.hyosik.android.pedometer.listener.StepListener
-import com.hyosik.android.pedometer.utils.StepDetector
+import com.hyosik.android.pedometer.utils.Accelerometer
+import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() , SensorEventListener, StepListener {
-
-    private var simpleStepDetector: StepDetector? = null
-    private var sensorManager: SensorManager? = null
-    private var numSteps: Int = 0
-    private val TEXT_NUM_STEPS = "Number of Steps: "
+class MainActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityMainBinding
 
@@ -26,33 +17,19 @@ class MainActivity : AppCompatActivity() , SensorEventListener, StepListener {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         binding.lifecycleOwner = this
 
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        simpleStepDetector = StepDetector()
-        simpleStepDetector!!.registerListener(this)
-
         binding.btnStart.setOnClickListener {
-            numSteps = 0
-            sensorManager!!.registerListener(this, sensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST)
+            Accelerometer.startStep()
+            Accelerometer.registerSensorListener(this)
         }
 
         binding.btnStop.setOnClickListener {
-            sensorManager!!.unregisterListener(this)
+            Accelerometer.unRegisterSensorListener()
         }
 
-    }
-
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-    }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        if (event!!.sensor.type == Sensor.TYPE_ACCELEROMETER) {
-            simpleStepDetector!!.updateAccelerometer(event.timestamp, event.values[0], event.values[1], event.values[2])
+        lifecycleScope.launch {
+            Accelerometer.numStepFLow.collect {
+                 binding.tvSteps.text = it.toString()
+            }
         }
     }
-
-    override fun step(timeNs: Long) {
-        numSteps++
-        binding.tvSteps.text = TEXT_NUM_STEPS.plus(numSteps)
-    }
-
 }
--- app/src/main/java/com/hyosik/android/pedometer/utils/Accelerometer.kt
+++ app/src/main/java/com/hyosik/android/pedometer/utils/Accelerometer.kt
@@ -1,4 +1,52 @@
 package com.hyosik.android.pedometer.utils
 
-object Accelerometer {
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import com.hyosik.android.pedometer.listener.StepListener
+import kotlinx.coroutines.flow.MutableStateFlow
+
+object Accelerometer : SensorEventListener, StepListener {
+
+    private var simpleStepDetector: StepDetector? = null
+    private var sensorManager: SensorManager? = null
+
+    private val _numStepFlow : MutableStateFlow<Int> = MutableStateFlow(0)
+    val numStepFLow
+        get() = _numStepFlow
+
+    init {
+        simpleStepDetector = StepDetector().apply { registerListener(this@Accelerometer) }
+    }
+
+    override fun onSensorChanged(event : SensorEvent?) {
+        if (event!!.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            simpleStepDetector!!.updateAccelerometer(event.timestamp, event.values[0], event.values[1], event.values[2])
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
+
+    override fun step(timeNs: Long) {
+        _numStepFlow.value += 1
+    }
+
+    fun registerSensorListener(context: Context) {
+        sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager?.let{
+            it.registerListener(this, it.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST)
+        }
+    }
+
+    fun unRegisterSensorListener() {
+        sensorManager?.unregisterListener(this)
+        sensorManager = null
+    }
+
+    fun startStep() {
+        _numStepFlow.value = 0
+    }
+
 }
